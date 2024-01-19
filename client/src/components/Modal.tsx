c1505fc884d19e27a7db101f71c9af8b51901ec4
SuyogJo <60322402+SuyogJo@users.noreply.github.com>
2024-01-19 18:32:42 -0500

    deleted unused import

--- client/src/components/Modal.tsx
+++ client/src/components/Modal.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import { useRef, useEffect } from 'react';
 import { Transition } from '@headlessui/react';
 
 const Modal = ({ isOpen, onClose}: {isOpen: boolean, onClose:any}) => {
